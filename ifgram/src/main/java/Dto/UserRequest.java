d518f4be1c20add4d2337faa7d1c14fb4c5accec
Bruno Messias da Silva <202412060078@ad.ifpb.edu.br>
2026-09-16 10:30:08 -0300

    Continuaçao

---
+++ ifgram/src/main/java/Dto/UserRequest.java
@@ -0,0 +1,14 @@
+package Dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UserRequest (
+    @Email
+    String email,
+    @Size(min=3)
+    @NotNull
+    String senha
+
+){}
